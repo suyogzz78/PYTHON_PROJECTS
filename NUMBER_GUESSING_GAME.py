b9879da3e4f3a2767defdcfacbcd5f90e9873c11
suyogzz78 <sakurachhetri97@gmail.com>
2025-04-03 18:46:01 +0545

    number guessing game in py

--- NUMBER_GUESSING_GAME.py
+++ NUMBER_GUESSING_GAME.py
@@ -1,34 +1,60 @@
-# Creating a number guessing name
+import random
+from words import words
+from hangman_visual import lives_visual_dict
+import string
 
-import random  # Used to generate random numbers
 
-# Generate a random number between 1 and 100
-actual_number = random.randint(1, 100)
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
 
-# Initialize the number of attempts
-attempts = 0
+    return word.upper()
 
-print("Welcome to the Number Guessing Game!")
-print("Try to guess the number between 1 and 100.")
 
-while True:
-    try:
-  
-        guess = int(input("Enter your guess: "))
-        
-        attempts += 1#counts the number of attempt user takes
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
 
-        if guess < 1 or guess > 100:
-            print("Please enter a number between 1 and 100.")
-            continue
+    lives = 7
 
-        if guess == actual_number:
-            print(f"Congratulations! You guessed the number correctly in {attempts} attempts.")
-            break
-        elif guess > actual_number:
-            print("Your guess is too high.")
+    # getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives = lives - 1  # takes away a life if wrong
+                print('\nYour letter,', user_letter, 'is not in the word.')
+
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
+
         else:
-            print("Your guess is too low.")
-    except ValueError:
-        # Handle the case where the user enters a non-integer value
-        print("Invalid input. Please enter a valid integer.")
+            print('\nThat is not a valid letter.')
+
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print(lives_visual_dict[lives])
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
+
+
+if __name__ == '__main__':
+    hangman()
